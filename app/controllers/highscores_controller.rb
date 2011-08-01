900ea2a2d60690ccb3970f21594b55baf65900db
Mario Gonzalez <mariogonzalez@gmail.com>
2011-07-31 20:03:17 -0400

    Switched over to SASS, added EndScoreState

--- app/controllers/highscores_controller.rb
+++ app/controllers/highscores_controller.rb
@@ -1,2 +1,23 @@
 class HighscoresController < ApplicationController
+  def new
+    @level = Level.find(params[:level_id])
+    @highscore = @level.highscores.build
+    @highscore.user_id = current_user.id
+    @highscore.score = 123.2
+    @highscore.save
+
+    #raise @highscore.to_yaml
+    #redirect_to @level
+  end
 end
+
+#/**
+#class CommentsController < ApplicationController
+#  def create
+#    @video = Video.find params[:video_id]
+#    @comment = @video.comments.build params[:comment]
+#
+#    redirect_to @video
+#  end
+#end
+#*/
