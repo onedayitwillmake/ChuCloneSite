7088cf8e4a315490ee3b68fd10d58082c32ed415
onedayitwillmake <mariogonzalez@gmail.com>
2011-08-10 09:23:25 -0400

    Adding player-recorder

--- app/helpers/levels_helper.rb
+++ app/helpers/levels_helper.rb
@@ -1,16 +1,18 @@
 module LevelsHelper
 
+	# Checks to see if there is a current user for this session
 	def get_current_user
 		@current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+	end
 
+	# Checks the json for a component type by name
 	def has_component leveljson, component_name
 		found_component = false
 		leveljson["bodyList"].each do |item|
 			item["components"].each do |component|
-				 if component["displayName"] == component_name then
-					 found_component = true
-				 end
+				if component["displayName"] == component_name then
+					found_component = true
+				end
 			end
 		end
 		found_component
