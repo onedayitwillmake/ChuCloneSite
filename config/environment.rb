cb0f032a7b70adae65e2548c9b40ba1954591510
onedayitwillmake <mariogonzalez@gmail.com>
2011-09-04 18:06:30 -0400

    Setting OpenSSL::SSL::VERIFY_PEER to VERIFY_NONE

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,13 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+module OpenSSL
+	module SSL
+		remove_const :VERIFY_PEER
+	end
+end
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
 # Initialize the rails application
 ChuCloneSite::Application.initialize!
+
